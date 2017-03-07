e5272db9d4a2741f2e12a6ff330aec0190d30f0e
cqjix <373105324@qq.com>
2017-03-07 14:22:08 +0800

    更改登录界面

--- app/src/main/java/org/crazyit/auction/client/Login.java
+++ app/src/main/java/org/crazyit/auction/client/Login.java
@@ -13,10 +13,12 @@ import org.json.JSONObject;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import cn.bmob.v3.BmobQuery;
 import cn.bmob.v3.BmobUser;
@@ -26,25 +28,27 @@ import cn.bmob.v3.listener.SaveListener;
 
 public class Login extends BaseActivity
 {
+	Toolbar toolbar;
 	// 定义界面中两个文本框
 	EditText etName, etPass;
 	// 定义界面中两个按钮
-	Button bnLogin, bnCancel,bnResgister;
+	Button bnLogin;
+	//注册
+	TextView tvRegister;
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.login);
+		toolbar= (Toolbar) findViewById(R.id.toolbar);
+		initToolbar(toolbar,getString(R.string.login));
 		// 获取界面中两个编辑框
 		etName = (EditText) findViewById(R.id.userEditText);
 		etPass = (EditText) findViewById(R.id.pwdEditText);
 		// 获取界面中的两个按钮
-		bnLogin = (Button) findViewById(R.id.bnLogin);
-		bnCancel = (Button) findViewById(R.id.bnCancel);
-		bnResgister= (Button) findViewById(R.id.bnRegister);
-		// 为bnCancal按钮的单击事件绑定事件监听器
-		bnCancel.setOnClickListener(new HomeListener(this));
-		bnResgister.setOnClickListener(new OnClickListener() {
+		bnLogin = (Button) findViewById(R.id.btn_login);
+		tvRegister= (TextView) findViewById(R.id.tv_register);
+		tvRegister.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View view) {
 				startActivity(new Intent(Login.this,Register.class));
@@ -59,21 +63,6 @@ public class Login extends BaseActivity
 				if (validate())  // ①
 				{
 					loginPro();
-//					// 如果登录成功
-//					if (loginPro())  // ②
-//					{
-//						// 启动Main Activity
-//						Intent intent = new Intent(Login.this
-//								, AuctionClientActivity.class);
-//						startActivity(intent);
-//						// 结束该Activity
-//						finish();
-//					}
-//					else
-//					{
-//						DialogUtil.showDialog(Login.this
-//								, "用户名称或者密码错误，请重新输入！", false);
-//					}
 				}
 			}
 		});
@@ -103,27 +92,11 @@ public class Login extends BaseActivity
 					//通过BmobUser user = BmobUser.getCurrentUser()获取登录成功后的本地用户信息
 					//如果是自定义用户对象MyUser，可通过MyUser user = BmobUser.getCurrentUser(MyUser.class)获取自定义用户信息
 				}else{
-					toast("登陆失败,请检查账号和密码");
+					toast("登陆失败,请检查账号和密码"+e.getMessage());
 					LogUtils.loge(e.getMessage());
 				}
 			}
 		});
-//		JSONObject jsonObj;
-//		try
-//		{
-//			jsonObj = query(username, pwd);
-//			// 如果userId 大于0
-//			if (jsonObj.getInt("userId") > 0)
-//			{
-//				return true;
-//			}
-//		}
-//		catch (Exception e)
-//		{
-//			DialogUtil.showDialog(this
-//					, "服务器响应异常，请稍后再试！", false);
-//			e.printStackTrace();
-//		}
 
 
 	}
@@ -146,17 +119,4 @@ public class Login extends BaseActivity
 		return true;
 	}
 
-	// 定义发送请求的方法
-	private JSONObject query(String username, String password)
-			throws Exception
-	{
-		// 使用Map封装请求参数
-		Map<String, String> map = new HashMap<>();
-		map.put("user", username);
-		map.put("pass", password);
-		// 定义发送请求的URL
-		String url = HttpUtil.BASE_URL + "login.jsp";
-		// 发送请求
-		return new JSONObject(HttpUtil.postRequest(url, map));
-	}
 }
